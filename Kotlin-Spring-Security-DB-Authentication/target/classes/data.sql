6326946e1339e0830dc23f613a7602efe507fcc1
spdeepak <spdeepak@outlook.com>
2018-08-08 17:34:27 +0200

    removed compiled files

--- Kotlin-Spring-Security-DB-Authentication/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user (first_name, last_name, pass_word, user_name, version, email)
-VALUES ('deepak', 'sp', '$2a$10$2nU5xMCyzPMeNiGQsrjzQeWNcHf9NjtGzVFgy6kVRcZlLh/ABTgZW', 'spdeepak', 1, "speedpak1991@gmail.com");
-INSERT INTO user (first_name, last_name, pass_word, user_name, version, email)
-VALUES ('deepak', 'sp', '$2a$10$2nU5xMCyzPMeNiGQsrjzQeWNcHf9NjtGzVFgy6kVRcZlLh/ABTgZW', 'deepak', 1, "speedpak1991@gmail.com");
