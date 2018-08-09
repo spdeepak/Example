4fa8f7ca671ed418204e1bae4b0631a8eba233ff
spdeepak <spdeepak@outlook.com>
2018-08-09 18:43:53 +0200

    used lombok

--- Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/entity/Role.java
+++ Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/entity/Role.java
@@ -1,31 +1,25 @@
 package com.github.spdeepak.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Role {
 
-	@Id
-	@GeneratedValue
-	private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
-	private String Role;
+    private String Role;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getRole() {
-		return Role;
-	}
-
-	public void setRole(String role) {
-		Role = role;
-	}
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
 }
