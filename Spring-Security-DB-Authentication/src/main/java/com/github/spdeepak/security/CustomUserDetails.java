55c54d8d0148678d83edb23dc2319a5622d38292
spdeepak <spdeepak@outlook.com>
2018-08-10 15:13:57 +0200

    used lombok

--- Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/security/CustomUserDetails.java
+++ Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/security/CustomUserDetails.java
@@ -1,8 +1,7 @@
 package com.github.spdeepak.security;
 
 import com.github.spdeepak.entity.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,52 +9,51 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class CustomUserDetails extends User implements UserDetails {
 
-	private static final Logger log = LoggerFactory.getLogger(CustomUserDetails.class);
+    public CustomUserDetails(final User user) {
+        super(user);
+    }
 
-	public CustomUserDetails(final User user) {
-		super(user);
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return super.getRoles()
+                .stream()
+                .map(role -> {
+                    log.debug("Granting Authority to user(" + super.getUsername() + ") with role: " + role.toString());
+                    return new SimpleGrantedAuthority(role.toString());
+                })
+                .collect(Collectors.toList());
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return getRoles()
-				.stream()
-				.map(role -> {
-					log.debug("Granting Authority to user with role: " + role.toString());
-					return new SimpleGrantedAuthority(role.toString());
-				})
-				.collect(Collectors.toList());
-	}
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
 
-	@Override
-	public String getPassword() {
-		return super.getPassword();
-	}
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
 
-	@Override
-	public String getUsername() {
-		return super.getUsername();
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return super.isAccountNonExpired();
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return super.isAccountNonExpired();
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return super.isAccountNonLocked();
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return super.isAccountNonLocked();
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return super.isCredentialsNonExpired();
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return super.isCredentialsNonExpired();
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return super.isEnabled();
-	}
+    @Override
+    public boolean isEnabled() {
+        return super.isEnabled();
+    }
 }
--- Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/service/CustomUserDetailsService.java
+++ Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.github.spdeepak.service;
 import com.github.spdeepak.entity.User;
 import com.github.spdeepak.repository.UserRepository;
 import com.github.spdeepak.security.CustomUserDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,16 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Override
-	@Transactional(readOnly = true)
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> optionalUser = userRepository.findByUsername(username);
-		optionalUser.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-		return optionalUser.map(CustomUserDetails::new).get();
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        optionalUser.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUser.map(CustomUserDetails::new).get();
+    }
 }
