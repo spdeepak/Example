d1ef0b3385ee6136a2fdd6eeb0c0dfc24006dec0
spdeepak <spdeepak@outlook.com>
2018-08-10 15:14:42 +0200

    used lombok and added logging

--- Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/service/UserService.java
+++ Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/service/UserService.java
@@ -2,10 +2,14 @@ package com.github.spdeepak.service;
 
 import com.github.spdeepak.entity.User;
 import com.github.spdeepak.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class UserService {
 
     private UserRepository userRepository;
@@ -17,7 +21,8 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User save(User user){
+    public User save(User user) {
+        log.info("Creating User with username: " + user.getUsername());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
