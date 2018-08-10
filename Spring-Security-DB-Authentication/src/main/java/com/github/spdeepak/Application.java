5d47afe0d2946bbb62e3e1c5a092cdc2e8c5fdd2
spdeepak <spdeepak@outlook.com>
2018-08-10 15:16:11 +0200

    refactor and property change

--- Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/Application.java
+++ Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/Application.java
@@ -9,12 +9,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
