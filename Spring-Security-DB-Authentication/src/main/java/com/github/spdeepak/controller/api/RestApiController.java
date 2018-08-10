531e703f0eccfd45d134331f942a92cc9991bbd9
spdeepak <spdeepak@outlook.com>
2018-08-10 15:15:03 +0200

    added rest controller

---
+++ Spring-Security-DB-Authentication/src/main/java/com/github/spdeepak/controller/api/RestApiController.java
@@ -0,0 +1,17 @@
+package com.github.spdeepak.controller.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+    @GetMapping
+    public ResponseEntity dummyController() {
+        return new ResponseEntity<>("This is the secured message", HttpStatus.OK);
+    }
+}
