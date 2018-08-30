bd4a6341f95a8f326d95a82101e78626f49a832e
spdeepak <spdeepak@outlook.com>
2018-08-30 17:41:00 +0200

    Spring boot admin client and server project added

---
+++ SpringBootAdmin/client/src/main/java/com/deepaksp/server/SpringBootClientApplication.java
@@ -0,0 +1,12 @@
+package com.deepaksp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientApplication.class, args);
+	}
+}
---
+++ SpringBootAdmin/client/src/main/java/com/deepaksp/server/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.deepaksp.server.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.formLogin()
+				.loginPage("/login.html")
+				.loginProcessingUrl("/login")
+				.permitAll();
+		http
+				.logout().logoutUrl("/logout");
+		http
+				.authorizeRequests()
+				.antMatchers("/api/1/*")
+				.authenticated()
+				.anyRequest()
+				.permitAll();
+		http
+				.csrf().disable();
+		http
+				.authorizeRequests()
+				.antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+				.permitAll();
+		http
+				.authorizeRequests()
+				.antMatchers("/**")
+				.authenticated();
+		http.httpBasic();
+	}
+}
