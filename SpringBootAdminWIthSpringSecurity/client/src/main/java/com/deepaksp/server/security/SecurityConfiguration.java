44b45467723dac59adaf64a4a5e87b07cdf7ce4d
spdeepak <spdeepak@outlook.com>
2018-08-31 11:58:43 +0200

    reformat and refactor security config in spring boot admin client security

--- SpringBootAdminWIthSpringSecurity/client/src/main/java/com/deepaksp/server/security/SecurityConfiguration.java
+++ SpringBootAdminWIthSpringSecurity/client/src/main/java/com/deepaksp/server/security/SecurityConfiguration.java
@@ -1,40 +1,33 @@
 package com.deepaksp.server.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("admin").password("admin").roles("ADMIN");
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.formLogin()
-				.loginPage("/login.html")
-				.loginProcessingUrl("/login")
-				.permitAll();
-		http
-				.logout().logoutUrl("/logout");
-		http
-				.authorizeRequests()
-				.antMatchers("/api/1/*")
-				.authenticated()
-				.anyRequest()
-				.permitAll();
-		http
-				.csrf()
-				.disable();
-		http
-				.authorizeRequests()
-				.antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-				.permitAll();
-		http
-				.authorizeRequests()
-				.antMatchers("/**")
-				.authenticated();
+		http.csrf().disable();
+		http.formLogin().permitAll();
+		http.logout().logoutUrl("/logout");
+		http.authorizeRequests().antMatchers("/manage", "/manage/**").permitAll();
+		http.authorizeRequests().antMatchers("/**").hasAnyRole("ADMIN");
 		http.httpBasic();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.exceptionHandling().authenticationEntryPoint((req, resp, e) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED));
 	}
 }
