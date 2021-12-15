ceb14e544d72459f36f03794e37bc4409513d3d4
Deepak <speedpak1991@gmail.com>
2021-12-15 08:25:11 +0100

    Start with DBAuth project

---
+++ Java/Spring/DBAuth/src/main/java/com/github/spdeepak/dbauth/DbAuthApplication.java
@@ -0,0 +1,13 @@
+package com.github.spdeepak.dbauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbAuthApplication.class, args);
+    }
+
+}
