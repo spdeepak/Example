3fe0e455865494c4dac24a017e3205ce597137e6
Deepak <speedpak1991@gmail.com>
2021-12-18 09:30:37 +0100

    fix return type and remote unwanted test

--- Java/Spring/Security-DB-Auth/src/main/java/com/github/spdeepak/security/api/RestApiController.java
+++ Java/Spring/Security-DB-Auth/src/main/java/com/github/spdeepak/security/api/RestApiController.java
@@ -1,6 +1,5 @@
 package com.github.spdeepak.security.api;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestApiController {
 
     @GetMapping
-    public ResponseEntity dummyController() {
-        return new ResponseEntity<>("This is the secured message", HttpStatus.OK);
+    public ResponseEntity<String> dummyController() {
+        return ResponseEntity.ok("This is the secured message");
     }
 
 }
